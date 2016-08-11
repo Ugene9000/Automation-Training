eba2d2a710c3257d46b9f2cbda618455eff2ec0d
Eugene Zubritsky <eugene_z@eugenes-mbp.by-minsk.csi.local>
2016-08-11 19:18:46 +0300

    optimized imports and renamed method name in OfficePageTest class

--- Task 60 - Patterns/src/test/java/OfficePageTest.java
+++ Task 60 - Patterns/src/test/java/OfficePageTest.java
@@ -1,11 +1,6 @@
 import base.TestBase;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.Test;
 import pages.OfficePage;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by eugene_z on 8/11/16.
@@ -20,7 +15,7 @@ public class OfficePageTest extends TestBase {
     }
 
     @Test
-    public void checkSearchByOfficeInput2() {
+    public void checkSearchByOfficeInput() {
         OfficePage officePage = new OfficePage(getDriver());
         officePage.loadPage();
         waitWithPollingFrequency(2700, 15, officePage.getSearchByOffice());
